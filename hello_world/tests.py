6abd97eaf550d5eb3e61fedf647489b52ecef2fa
dipsprajapati <dips.p.007@gmail.com>
2020-05-31 14:11:35 +0800

    added travis file

--- hello_world/tests.py
+++ hello_world/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class HelloWorldTestClass(TestCase):
+
+    def test_hello_world_is_displayed(self):
+
+        print('Test has been run successfully')
